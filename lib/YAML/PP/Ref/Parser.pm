6b9aa96592926a98245b26c0feb3c3aad58dd46b
Tina Müller <cpan2@tinita.de>
2021-12-28 19:50:09 +0100

    Transform event stucture for YAML::PP

--- lib/YAML/PP/Ref/Parser.pm
+++ lib/YAML/PP/Ref/Parser.pm
@@ -4,8 +4,23 @@ use warnings;
 
 use Scalar::Util qw/ openhandle /;
 use YAML::Parser;
+use YAML::PP::Common qw(
+    YAML_PLAIN_SCALAR_STYLE
+    YAML_SINGLE_QUOTED_SCALAR_STYLE YAML_DOUBLE_QUOTED_SCALAR_STYLE
+    YAML_LITERAL_SCALAR_STYLE YAML_FOLDED_SCALAR_STYLE
+    YAML_BLOCK_SEQUENCE_STYLE YAML_FLOW_SEQUENCE_STYLE
+    YAML_BLOCK_MAPPING_STYLE YAML_FLOW_MAPPING_STYLE
+);
 
 use base 'YAML::PP::Parser';
+
+my %style_map = (
+  plain   => YAML_PLAIN_SCALAR_STYLE,
+  single  => YAML_SINGLE_QUOTED_SCALAR_STYLE,
+  double  => YAML_DOUBLE_QUOTED_SCALAR_STYLE,
+  literal => YAML_LITERAL_SCALAR_STYLE,
+  folded  => YAML_FOLDED_SCALAR_STYLE,
+);
 
 sub parse {
     my ($self) = @_;
@@ -28,8 +43,39 @@ sub parse {
 
     my $cb = sub {
         my ($info) = @_;
-        $info->{event} .= '_event';
-        my $event = $info->{event};
+        # Transform events becayse YAML::Parser uses something
+        # very differnt from libyaml and YAML::PP
+        my $event = (delete $info->{event}) . '_event';
+        if ($event eq 'alias_event') {
+            $info->{value} = delete $info->{name};
+        }
+        elsif ($event eq 'scalar_event') {
+            my $style = $style_map{ $info->{style} };
+            $info->{style} = $style;
+        }
+        elsif ($event eq 'document_start_event') {
+            $info->{implicit} = delete $info->{explicit} ? 0 : 1;
+        }
+        elsif ($event eq 'document_end_event') {
+            $info->{implicit} = delete $info->{explicit} ? 0 : 1;
+        }
+        elsif ($event eq 'sequence_start_event') {
+            if (delete $info->{flow}) {
+                $info->{style} = YAML_FLOW_SEQUENCE_STYLE;
+            }
+            else {
+                $info->{style} = YAML_BLOCK_SEQUENCE_STYLE;
+            }
+        }
+        elsif ($event eq 'mapping_start_event') {
+            if (delete $info->{flow}) {
+                $info->{style} = YAML_FLOW_MAPPING_STYLE;
+            }
+            else {
+                $info->{style} = YAML_BLOCK_MAPPING_STYLE;
+            }
+        }
+        $info->{name} = $event;
         return $co->$event($info);
     };
     my $refrec = PerlYamlReferenceParserReceiver->new(
